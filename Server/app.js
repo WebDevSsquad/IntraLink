fa3b6725f3bbf4f8b45d5cbd34e03bcd42171478
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2023-12-12 21:48:54 +0200

    connecting to render

---
+++ Server/app.js
@@ -0,0 +1,62 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import pkg from "pg";
+const { Pool } = pkg;
+//import authMiddleware from "./middlewares/auth.middleware.js";
+//import authRouter from "./routers/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+//--------------- connecting to the database-----------------
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("Error connecting to PostgreSQL:", err);
+    return;
+  }
+
+  console.log("Connected to PostgreSQL database!");
+
+  // Release the client to the pool
+  release();
+  pool.end(); // Close the pool after the connection check
+});
+
+//  pool.query(`insert into public."User" (username,email,password,firstname,lastname)  values ('the fearmaker','midoamr20@gmail.com','jashdflkjhasdkfjhaskljdfh48456s4ad','muhammad','amr');`, (err, result) => {
+//   if (err) {
+//     console.error("Error executing query:", err.stack);
+//     return;
+//   }
+//   console.log("Result:", result.rows);
+// });
+
+
+//app.use("/auth", authRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, (err) => {
+  if (err) return console.error(err);
+  console.log(`Server started listening at port ${PORT}`);
+});
---
+++ Server/routers/auth.route.js
@@ -0,0 +1,10 @@
+// import { Router } from "express";
+// import authController from "../controllers/auth.controller.js";
+// import authMiddleware from "../middlewares/auth.middleware.js";
+// const authRouter = Router();
+
+// authRouter.post("/signUp", authController.signup);
+// authRouter.post("/logIn", authController.login);
+// authRouter.post("/image", authController.updateImage);
+// authRouter.get("/me", authMiddleware, authController.me);
+// export default authRouter;
