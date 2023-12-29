9575f8fa62a5b4cc356ce1a4501cfb76603b8659
YoussefMehany <117949033+YoussefMehany@users.noreply.github.com>
2023-12-29 23:24:37 +0200

    final

--- Server/db.js
+++ Server/db.js
@@ -1,8 +1,8 @@
 // db.js
 import pkg from "pg";
 const { Pool } = pkg;
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
